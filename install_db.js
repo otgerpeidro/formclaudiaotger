91ace77b55ee48350aa02f55a6a265944ae6369b
Otger Peidro <otgerpeidro@gmail.com>
2017-11-26 17:41:05 +0100

    Update install_db.js

--- install_db.js
+++ install_db.js
@@ -116,7 +116,7 @@ function initLibro(cb) {
     Usuario.remove({}, ()=> {
   
       const usuarios = [
-        { name: 'admin', email: 'otgerpeidro@hotmail.com', pass: 'Girona090618' }
+        { }
       ];
   
       async.eachSeries(usuarios, Usuario.createRecord, (err)=> {
